49e8c2ed8984e844f7e9b58249dd7a9a1541dbe0
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-01 14:50:23 -0500

    Adding deploy script

--- ContactForm/js/index.js
+++ ContactForm/js/index.js
@@ -4,5 +4,13 @@ var app = {
 	 */
 	initialize: function() {
 		
+	},
+	/**
+	 *
+	 */
+	getBrowserLanguage() {
+		var language = window.navigator.userLanguage || window.navigator.language;
+		
+		return language;
 	}
 };
