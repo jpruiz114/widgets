c2a5d0c921e28e427bbf78a74f8602439eabad6e
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-01 15:18:20 -0500

    Test

--- ContactForm/js/index.js
+++ ContactForm/js/index.js
@@ -1,12 +1,15 @@
 var app = {
 	/**
-	 *
+	 * Method that initializes the app.
 	 */
 	initialize: function() {
+		var language = app.getBrowserLanguage();
+		console.log("language" + " = " + language);
+		
 		
 	},
 	/**
-	 *
+	 * Method that retrieves the current navigator language.
 	 */
 	getBrowserLanguage() {
 		var language = window.navigator.userLanguage || window.navigator.language;
