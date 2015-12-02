ae5a8b4e2b181e03d586c73d5b5f52b56f0170f4
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-01 22:47:24 -0500

    Generic comment

--- ContactForm/index.php
+++ ContactForm/index.php
@@ -88,7 +88,7 @@ if (!empty($params)) {
 					<label data-i18n="contact-form.contacts"></label>
 				</div>
 				
-				<div class="full-size-row extra-space">
+				<div class="full-size-row">
 					
 				</div>
 				
@@ -96,7 +96,7 @@ if (!empty($params)) {
 					<label data-i18n="contact-form.subject"></label>
 				</div>
 				
-				<div class="full-size-row extra-space">
+				<div class="full-size-row">
 					<input data-i18n="[placeholder]contact-form.add-subject" id="subject" type="text" />
 				</div>
 				
@@ -104,7 +104,7 @@ if (!empty($params)) {
 					<label data-i18n="contact-form.message"></label>
 				</div>
 				
-				<div class="full-size-row extra-space">
+				<div class="full-size-row">
 					<textarea data-i18n="[placeholder]contact-form.leave-message" id="message"></textarea>
 				</div>
 				
