e9619ee5a43948a991059ac1db198c2adfcc5c4c
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-01 19:39:57 -0500

    Test

--- ContactForm/index.php
+++ ContactForm/index.php
@@ -1,3 +1,11 @@
+<?php
+// remove the directory path we don't want
+$request = str_replace("/envato/pretty/php/", "", $_SERVER['REQUEST_URI']);
+
+// split the path by '/'
+$params = split("/", $request);
+print_r($params);
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
