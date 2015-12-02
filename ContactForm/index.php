34be8e71192e60676b9ac02fcc53572145b20d18
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-01 20:31:40 -0500

    Generic comment

--- ContactForm/index.php
+++ ContactForm/index.php
@@ -41,6 +41,8 @@ if (!empty($params)) {
 		<script type="text/javascript" src="components/toastr/toastr.min.js"></script>
 		
 		<script type="text/javascript" src="components/i18next/i18next.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="components/flag-icon-css/flag-icon.min.css">
 		
 		<script type="text/javascript" src="js/index.js"></script>
 		
