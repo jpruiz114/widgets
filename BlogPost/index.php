597733726e7973770371c8615db0911be7594cb0
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-03 14:19:36 -0500

    Automatic build, validation and commit done by shell script

--- BlogPost/index.php
+++ BlogPost/index.php
@@ -1,3 +1,18 @@
+<?php
+// Remove the directory path we don't want
+$request = str_replace("/frontend-test/ContactForm/", "", $_SERVER["REQUEST_URI"]);
+
+// Split the path by "/"
+$params = explode("/", $request);
+
+$lang = "";
+
+if (!empty($params)) {
+    if (sizeof($params) >= 1) {
+        $lang = $params[0];
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
     <head lang="en">
@@ -7,6 +22,42 @@
     </head>
 
     <body>
+        <div id="wrapper">
+            <div id="top-bar">
+                <ul class="langs">
+                    <li class="langs-item">
+                        <a class="langs-link" href="/frontend-test/BlogPost/en" id="go-to-en-page" target="_self" data-i18n="[title]blog-post.english-website;blog-post.english-website"></a>
+                    </li>
 
+                    <li class="langs-item">
+                        <a class="langs-link" href="/frontend-test/BlogPost/es" id="go-to-es-page" target="_self" data-i18n="[title]blog-post.spanish-website;blog-post.spanish-website"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="post-container">
+                <div id="post-picture">
+
+                </div>
+
+                <div id="post-author">
+
+                </div>
+
+                <div id="post-options">
+                    <div class="post-option-column corner-option-column">
+
+                    </div>
+
+                    <div class="post-option-column central-option-column">
+
+                    </div>
+
+                    <div class="post-option-column corner-option-column">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
