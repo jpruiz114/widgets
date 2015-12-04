4f93b1c2eff9815cca52280d5ee291d2bbbe757e
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-03 21:47:11 -0500

    Automatic build, validation and commit done by shell script

--- BlogPost/index.php
+++ BlogPost/index.php
@@ -51,6 +51,22 @@ if (!empty($params)) {
                     var chosenLang = "<?php echo($lang); ?>";
 
                     app.initialize(chosenLang);
+
+                    //
+
+                    var count = true;
+                    
+                    $("div.like").click(function(){
+                        var heart = $('<div class="post_animated_heart post_poof"><span class="heart_left"></span><span class="heart_right"></span></div>').toggleClass("unliked", count = !count);
+
+                        $(this).toggleClass("liked").append(heart);
+
+                        setTimeout(function() {
+                            heart.fadeOut(200, function() {
+                                heart.remove()
+                            })
+                        }, 300);
+                    });
                 }
             );
         </script>
