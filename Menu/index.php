6bb1ba3c183e82e9dd8571a1fca82ee8bf82ce98
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-05 14:51:21 -0500

    Automatic build, validation and commit done by shell script

--- Menu/index.php
+++ Menu/index.php
@@ -60,7 +60,7 @@ $basePath = "/frontend-test/Menu/";
                     <a href="#">
                         <i class="fa fa-map-marker"></i>
                         Check in
-                        <div class="circle">3</div>
+                        <div class="circle-bubble">3</div>
                     </a>
                 </li>
 
