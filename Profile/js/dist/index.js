f1916460079b5814bc2f17e2e325d9e50d2c0dfa
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-05 17:03:27 -0500

    Automatic build, validation and commit done by shell script

--- Profile/js/dist/index.js
+++ Profile/js/dist/index.js
@@ -4,5 +4,42 @@ var app = {
      */
     initialize: function (chosenLang) {
         $("#profile-container").show();
+    },
+
+    /**
+     * Function that validates if the requested language is actually supported. If not, provides the default one.
+     * @param chosenLang
+     */
+    failSafeChosenLang: function(chosenLang) {
+        var resultLang;
+
+        if (chosenLang != app.LANGUAGE_CODE_FOR_ENGLISH && chosenLang != app.LANGUAGE_CODE_FOR_SPANISH) {
+            resultLang = app.DEFAULT_LANGUAGE;
+        } else {
+            resultLang = chosenLang;
+        }
+
+        return resultLang;
+    },
+
+    /**
+     *
+     */
+    basePath: "",
+
+    /**
+     *
+     * @param basePath
+     */
+    setBasePath: function(basePath) {
+        this.basePath = basePath;
+    },
+
+    /**
+     *
+     * @returns {string}
+     */
+    getBasePath: function() {
+        return this.basePath;
     }
 };
