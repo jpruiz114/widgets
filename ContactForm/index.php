a2a15bd3b35e6a0ff9c2dc3792c8f3b733ff01b2
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-05 12:47:01 -0500

    Automatic build, validation and commit done by shell script

--- ContactForm/index.php
+++ ContactForm/index.php
@@ -22,7 +22,7 @@ if (!empty($params)) {
 
 		<title>Contact Form</title>
 
-		<meta name="viewport" content="width=device-width, initial-scale=0.75">
+		<meta name="viewport" content="width=device-width, initial-scale=0.8">
 
 		<meta name="author" content="Jean Paul Ruiz">
 		
