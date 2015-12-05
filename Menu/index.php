d557ac25bd3bce4b1e0a5e72050aa636f3756ec2
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-12-05 12:07:57 -0500

    Automatic build, validation and commit done by shell script

--- Menu/index.php
+++ Menu/index.php
@@ -1,17 +1,53 @@
 <?php
 $basePath = "/frontend-test/Menu/";
-
-// Remove the directory path we don't want
-$request = str_replace($basePath, "", $_SERVER["REQUEST_URI"]);
-
-// Split the path by "/"
-$params = explode("/", $request);
-
-$lang = "";
-
-if (!empty($params)) {
-    if (sizeof($params) >= 1) {
-        $lang = $params[0];
-    }
-}
 ?>
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+
+        <title>Menu</title>
+
+        <meta name="author" content="Jean Paul Ruiz">
+
+        <meta name="description" content="This is an example of a menu">
+
+        <meta name="keywords" content="menu">
+
+        <meta name="robots" content="index, follow">
+
+        <meta name="revisit-after" content="7 days">
+
+        <link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+
+        <!-- PT Sans is a close match to the popular Myriad Pro -->
+        <link href='//fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" type="text/css" href="<?php echo $basePath; ?>components/fontawesome/css/font-awesome.min.css">
+
+        <link rel="stylesheet" type="text/css" href="<?php echo $basePath; ?>css/index.css">
+
+        <script type="text/javascript" src="<?php echo $basePath; ?>components/jquery/dist/jquery.min.js"></script>
+
+        <script type="text/javascript" src="<?php echo $basePath; ?>js/dist/index.js"></script>
+
+        <script type="text/javascript">
+            /**
+             * When all the website resources (Images, scripts) have been loaded.
+             */
+            $(window).load(
+                function() {
+                    app.initialize();
+                } // End of the function.
+            );
+        </script>
+
+        <?php include_once("../ga-tracking.php") ?>
+    </head>
+
+    <body>
+        <div id="wrapper">
+
+        </div>
+    </body>
+</html>
